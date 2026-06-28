0e9bb25f440c29f2979e66d108348f806461194b
ILearnToCodeHooray <134112193+ILearnToCodeHooray@users.noreply.github.com>
2026-06-28 19:59:43 +0000

    day56games

--- lessons/07_Projects/05_Lunar_Lander/main.py
+++ lessons/07_Projects/05_Lunar_Lander/main.py
@@ -15,6 +15,8 @@ class Colors:
 
 class Settings:
     """Settings for the game"""
+    font = pygame.font.SysFont(None, 36)
+    colors = {"white": (255, 255, 255), "black": (0, 0, 0), "red": (255, 0, 0), "blue": (0, 0, 255)}
     width: int = 600
     height: int = 400
     gravity: float = 0.3
@@ -26,7 +28,10 @@ class Settings:
     player_height: int = 30
     player_jump_velocity: float = 15
     frame_rate: int = 15
+    lives = 3
     move = pygame.Vector2(0, 1)
+    fuel = 100
+    score = 0
 
 class Game:
     """Main object for the top level of the game. Holds the main loop and other
@@ -48,19 +53,53 @@ class Game:
     def run(self):
         """Main game loop"""
         player = Player(self)
-
         while self.running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
                     self.running = False
 
+            if Settings.fuel == 0:
+                pygame.quit()
             player.update()
 
             self.screen.fill(Colors.BACKGROUND_COLOR)
             player.draw(self.screen)
+            fuel_text = Settings.font.render(f"Fuel: {(Settings.fuel)}", True, Settings.colors['black'])
+            self.screen.blit(fuel_text, (10, 10))
+            score_text = Settings.font.render(f"Score: {(Settings.score)}", True, Settings.colors['black'])
+            self.screen.blit(score_text, (150, 10))
+            lives_text = Settings.font.render(f"Lives: {(Settings.lives)}", True, Settings.colors['black'])
+            self.screen.blit(lives_text, (300, 10))
+            v1 = pygame.math.Vector2(0, 400)
+            v2 = pygame.math.Vector2(10, 350)
+            v3 = pygame.math.Vector2(50, 340)
+            v4 = pygame.math.Vector2(150, 280)
+            v5 = pygame.math.Vector2(200, 330)
+            v6 = pygame.math.Vector2(230, 330)
+            v7 = pygame.math.Vector2(280, 380)
+            v8 = pygame.math.Vector2(330, 380)
+            v9 = pygame.math.Vector2(400, 300)
+            v10 = pygame.math.Vector2(420, 320)
+            v11 = pygame.math.Vector2(470, 320)
+            v12 = pygame.math.Vector2(530, 270)
+            v13 = pygame.math.Vector2(600, 270)
+            pygame.draw.line(self.screen, Settings.colors['black'], v1, v2, 2)
+            pygame.draw.line(self.screen, Settings.colors['black'], v2, v3, 2)
+            pygame.draw.line(self.screen, Settings.colors['black'], v3, v4, 2)
+            pygame.draw.line(self.screen, Settings.colors['black'], v4, v5, 2)
+            pygame.draw.line(self.screen, Settings.colors['black'], v5, v6, 2)
+            pygame.draw.line(self.screen, Settings.colors['black'], v6, v7, 2)
+            pygame.draw.line(self.screen, Settings.colors['black'], v7, v8, 2)
+            pygame.draw.line(self.screen, Settings.colors['black'], v8, v9, 2)
+            pygame.draw.line(self.screen, Settings.colors['black'], v9, v10, 2)
+            pygame.draw.line(self.screen, Settings.colors['black'], v10, v11, 2)
+            pygame.draw.line(self.screen, Settings.colors['black'], v11, v12, 2)
+            pygame.draw.line(self.screen, Settings.colors['black'], v12, v13, 2)
+
+            if Settings.lives == 0:
+                pygame.quit()            
             pygame.display.flip()
             self.clock.tick(self.settings.frame_rate)
-
         pygame.quit()
 
 class Player(pygame.sprite.Sprite):
@@ -86,6 +125,7 @@ class Player(pygame.sprite.Sprite):
         
         # Player's velocity
         self.vel = pygame.Vector2(settings.player_v_x, settings.player_v_y)  # Velocity vector
+        self.rect = self.image.get_rect()
 
     def going_up(self):
         """Check if the player is going up"""
@@ -130,6 +170,18 @@ class Player(pygame.sprite.Sprite):
         self.vel += self.game.gravity  # Add gravity to the velocity
 
         if self.at_bottom() and self.going_down():
+            if self.vel.y < 6 and self.vel.y != 0.3:
+                Settings.score += 1
+                self.pos.x = Settings.player_start_x
+                self.pos.y = Settings.player_start_y
+            elif self.vel.y > 6:
+                Settings.lives -= 1
+                self.pos.x = Settings.player_start_x
+                self.pos.y = Settings.player_start_y
+            elif self.vel.x > 6:
+                Settings.lives -= 1
+                self.pos.x = Settings.player_start_x
+                self.pos.y = Settings.player_start_y
             self.vel.y = 0
             self.vel.x = 0 
 
@@ -170,18 +222,19 @@ class Player(pygame.sprite.Sprite):
         if pygame.key.get_pressed()[pygame.K_LEFT]:
             self.angle += 5
             Settings.move = pygame.Vector2(0,1)
-            Settings.move.rotate_ip(self.angle)
+            Settings.move.rotate_ip(-self.angle)
         if pygame.key.get_pressed()[pygame.K_RIGHT]:
             self.angle -= 5
             Settings.move = pygame.Vector2(0,1)
-            Settings.move.rotate_ip(self.angle)
-        print(Settings.move)
+            Settings.move.rotate_ip(-self.angle)
         if pygame.key.get_pressed()[pygame.K_UP]:
             self.vel -= Settings.move
+            Settings.fuel -= 1
+        if self.rect.clipline((200, 330), (230, 330)):
+            pygame.quit()
     def draw(self, screen):
         self.image_rotated = pygame.transform.rotate(self.image, self.angle)
         screen.blit(self.image_rotated, (self.pos.x, self.pos.y))
-
 settings = Settings()
 game = Game(settings)
 game.run()
